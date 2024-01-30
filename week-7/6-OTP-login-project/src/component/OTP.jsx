f93ebb992f85ffaf3b3035143258400a781d863a
krishna Sarma <krishsarma03@gmail.com>
2024-01-30 23:52:02 +0530

    OTP input box focus feature added

--- week-7/6-OTP-login-project/src/component/OTP.jsx
+++ week-7/6-OTP-login-project/src/component/OTP.jsx
@@ -1,20 +1,29 @@
+import { useRef } from "react"
 
 export default function OTP(){
+
+    const inputBoxes = Array.from({length: 4}, (_, index)=> useRef());
+
+    function inputHandler(index, value){
+        if(value.length === 1 && index < inputBoxes.length -1){
+            inputBoxes[index + 1].current.focus();
+        }
+    }
+
     return (
         <div className="flex h-screen">
         <div className="flex flex-col w-[450px] justify-center items-center border m-auto gap-7 pb-20 pt-16 rounded-xl bg-black">
             <h2 className="text-white text-4xl">Login via OTP</h2>
             <div className="flex gap-2">
-                <input className="w-[55px] h-[50px] bg-black border border-gray-400 text-gray-400 p-5 text-xl" type="text"></input>
-                <input className="w-[55px] h-[50px] bg-black border border-gray-400 text-gray-400 p-5 text-xl"></input>
-                <input className="w-[55px] h-[50px] bg-black border border-gray-400 text-gray-400 p-5 text-xl"></input>
-                <input className="w-[55px] h-[50px] bg-black border border-gray-400 text-gray-400 p-5 text-xl"></input>
+                {inputBoxes.map((ref,index)=>(
+                    <input className="w-[55px] h-[50px] bg-black border border-gray-400 text-gray-400 p-5 text-xl"
+                    key={index}
+                    ref={ref}
+                    maxLength={1} 
+                    onChange={(e)=>inputHandler(index, e.target.value)}>
+                    </input>
+                ))}
             </div>
-            {/* <input className=" bg-black border border-gray-400 w-[50%] p-1 pl-4 rounded-lg text-gray-400"
-                type="text" placeholder="Enter your mobile number"
-                onChange={(e)=>{
-                    isNaN(e.target.value) ? alert("Wrong input") : setInputNumber(parseInt(e.target.value))
-                    }}></input> */}
             <button className="text-white border border-gray-400 p-1 pl-3 pr-3 rounded-lg"
                 >
                 Login</button>
